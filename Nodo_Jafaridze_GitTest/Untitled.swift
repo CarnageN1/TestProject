c9657a6c5d6e92b59d5cac129d0af1d6222cf44e
NODO JAFARIDZE <carnage@MacBook-Air-NODO.local>
2025-02-21 01:39:10 +0400

    second commit upload

--- Nodo_Jafaridze_GitTest/Untitled.swift
+++ Nodo_Jafaridze_GitTest/Untitled.swift
@@ -8,3 +8,8 @@
 var ufcWeights: [String] = ["Flyweight", "Bantamweight", "Featherweight", "Lightweight", "Middleweight", "Light Heavyweight", "Heavyweight"]
 
 var ufcChampions: [String] = ["Pantoja", "Dvalisvhili", "Topuria", "Makachev", "Belal","Du Plesis", "Pereira", "Jon Jones"]
+
+// 7 weight, 8 fighter, 1 is missing
+
+
+var correctUfcWeights: [String] = ["Flyweight", "Bantamweight", "Featherweight", "Lightweight", "Welterweight", "Middleweight", "Light Heavyweight", "Heavyweight"]
