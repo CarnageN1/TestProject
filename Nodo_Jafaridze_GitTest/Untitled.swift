8113544a74111894930bf362cbe3d06c6f2c6b4a
NODO JAFARIDZE <carnage@MacBook-Air-NODO.local>
2025-02-21 01:12:27 +0400

    create test project

---
+++ Nodo_Jafaridze_GitTest/Untitled.swift
@@ -0,0 +1,8 @@
+//
+//  Untitled.swift
+//  Nodo_Jafaridze_GitTest
+//
+//  Created by NODO JAFARIDZE on 2/21/25.
+//
+
+var ufcWeights: [String] = ["Flyweight", "Bantamweight", "Featherweight", "Lightweight", "Middleweight", "Light Heavyweight", "Heavyweight"]
---
+++ Nodo_Jafaridze_GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nodo_Jafaridze_GitTest
+//
+//  Created by NODO JAFARIDZE on 2/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
